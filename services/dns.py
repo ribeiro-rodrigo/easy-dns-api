e7626e1039aacbba22aca071244f4a4594a8acc1
Rodrigo Ribeiro <ribeiro.rodrigo1989@gmail.com>
2019-11-02 22:32:45 -0300

    implementando metodo de transferencia de zona

--- services/dns.py
+++ services/dns.py
@@ -1,3 +1,7 @@
+import dns.zone
+import dns.query
+import dns.rdatatype
+
 from config.config_helper import ConfigHelper
 
 
@@ -7,7 +11,26 @@ class DNSService:
         self.__server_host = cfg.config['dns']['server_host']
 
     def transfer_zone(self, zone_name):
-        pass
+        transferred_zone = dns.zone.from_xfr(dns.query.xfr(self.__server_host, zone_name))
+        entries = {}
+
+        for name, ttl, rdata in transferred_zone.iterate_rdatas():
+            entry = self.__make_entry(ttl, rdata)
+
+            if entries.get(str(name)):
+                entry = entries[str(name)] + entry
+
+            entries[str(name)] = entry
+
+        return entries
+
+    @classmethod
+    def __make_entry(cls, ttl, rdata):
+        return [{
+            'type': dns.rdatatype.to_text(rdata.rdtype),
+            'ttl': str(ttl),
+            'answer': str(rdata)
+        }]
 
 
 
